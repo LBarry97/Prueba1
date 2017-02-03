c3283bc46141c3b8c4fde66372de2dfc896091a6
LBarry97 <lansanabarry97@gmail.com>
2017-02-03 16:36:48 +0100

    Update try.js

--- xml/try.js
+++ xml/try.js
@@ -4,7 +4,7 @@ var secret=0; // Inicializado a "0"
 // Al cargar la págin...
 window.onload = function(){
 	// pide los datos, lee preguntas.xml del servidor (por http)
-	var xhttp = XMLHttpRequest();
+	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function(){
 		if (this.readyState == 4 && this.status == 200){
 			gestionarXml(this);
